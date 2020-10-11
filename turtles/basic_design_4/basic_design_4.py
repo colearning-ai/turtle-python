b4249573bc9f95198d66e94d20cd758079d6d5e9
colearning.ai <72709008+colearning-ai@users.noreply.github.com>
2020-10-12 02:58:24 +0530

    Update basic_design_4.py

--- turtles/basic_design_4/basic_design_4.py
+++ turtles/basic_design_4/basic_design_4.py
@@ -1,12 +1,11 @@
 import turtle 
 from random import randint 
-import time
 
 turtle.speed(0)
 turtle.bgcolor('black')
 
 x = 1
-time.sleep(15)
+
 while x < 300:
     r = randint(0,255) #makes variables r,g,b whose value is an integer,
     g = randint(0,255) # which is between 0 and 255. It is random, and
